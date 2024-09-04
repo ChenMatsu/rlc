763354006ebf8d65ae33299987ea4e2988fc8c53
Matsu Chen <sonamumatsu0223@gmail.com>
2024-09-04 21:11:19 +0900

    build: added Makefile and syntax correction in sources

--- examples/tic_tac_toe_example.c
+++ examples/tic_tac_toe_example.c
@@ -1,9 +1,25 @@
-#include "tic_tac_toe.c"
+#include <stdio.h>
+// // #include "tic_tac_toe.c"
 
 int main() {
-    TicTacToeEnv* env = tic_tac_toe_init();
+    printf("Welcome to the tic-tac-toe game!\n");
+    printf("You are player X, try to win this game!\n");
+
+
+// 
+    // // TicTacToeEnv* env = tic_tac_toe_init();
+ //
+    // // while (tic_tac_toe_is(ti_done(env) == 0) {
+    //     tic_tac_toe_render(env);
+
+    //     int action = -1;
+    //     while (action < 0 || action > 8) {
+    //         printf("Choose your action: ");
+    //         scanf("%d", &action);
+    //     }
+    // }
 
     // TODO
-    
+        
     return 0;
 }
